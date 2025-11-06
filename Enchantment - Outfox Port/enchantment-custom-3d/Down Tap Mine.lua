ae782cab9db38a83b5566b14ac89e2bd7c5ebffd
PoRa-dayo <62925753+PoRa-dayo@users.noreply.github.com>
2025-11-06 16:03:36 +0700

    enchantment-3d and outfox

---
+++ Enchantment - Outfox Port/enchantment-custom-3d/Down Tap Mine.lua
@@ -0,0 +1,8 @@
+return Def.ActorFrame{ 
+    Def.Model {
+        Meshes=NOTESKIN:GetPath('','enchantmine_mesh'),
+        Materials=NOTESKIN:GetPath('_mine','materials'),
+        Bones=NOTESKIN:GetPath('_mine','bones')
+    },
+    
+}
